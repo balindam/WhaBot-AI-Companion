0507d81f2cc5b1b68a71c0129b15393414c48021
balindam <balindam.c@gmail.com>
2023-12-26 01:58:18 +0530

    feat: add GET method to verify callback URL

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,7 +3,21 @@ import {WebhookService} from '../service/service.js';
 export class WebhookController {
     constructor(app) {
         this.WebhookService = new WebhookService();
+        // to verify webhook callback URL
+        app.get('/webhook', this.verifyWebhookURL.bind(this));
         app.post('/webhook', this.processWebhook.bind(this));
+    }
+
+    verifyWebhookURL(req, res) {
+        const mode = req.query["hub.mode"];
+        const challenge = req.query["hub.challenge"];
+        const token = req.query["hub.verify_token"];
+
+        if(mode === 'subscribe' && token === process.env.WHATSAPP_CLOUD_API_CALLBACK_URL_TOKEN) {
+            res.status(200).send(challenge);
+        } else {
+            res.status(403);
+        }
     }
 
     processWebhook(req, res) {
